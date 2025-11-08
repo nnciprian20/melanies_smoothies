493c4e8429fbeccae2dea4fe66702383d73cfe3b
nnciprian20 <nnciprian@gmail.com>
2025-11-08 10:54:59 +0200

    add nutritional infor for each fruit choosen

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@
 import streamlit as st
 #from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
+import requests
 
 # Write directly to the app
 st.title(":cup_with_straw: Customize your smoothie ")
@@ -23,8 +24,11 @@ ingredients_list = st.multiselect(
 
 if ingredients_list:
     ingredients_string = ''
-    for x in ingredients_list:
-        ingredients_string += (x + ' ')
+    for fruit_choosen in ingredients_list:
+        ingredients_string += (fruit_choosen + ' ')
+        st.subheader(fruit_choosen + ' Nutritional Information ')
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + fruit_choosen)
+        sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
     my_insert_stmt = """INSERT INTO SMOOTHIES.PUBLIC.ORDERS(ingredients, name_on_order) 
                         VALUES('""" + ingredients_string + """', '""" + name_on_order + """')"""
@@ -35,9 +39,6 @@ if ingredients_list:
         session.sql(my_insert_stmt).collect()
         st.success('Your smoothie is ordered, '+ name_on_order +'!', icon = '✅')
 
-import requests
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-#st.text(smoothiefroot_response.json())
-sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
+
 
 
